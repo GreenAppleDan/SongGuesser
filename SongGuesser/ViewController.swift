47f4bd502358b516f921df551a33d3c28cc3e0be
Denis Zhukoborsky <denibogda@gmail.com>
2019-07-28 17:32:39 +0300

    Initial Commit

---
+++ SongGuesser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SongGuesser
+//
+//  Created by Денис Жукоборский on 28/07/2019.
+//  Copyright © 2019 Денис Жукоборский. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
